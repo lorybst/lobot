ea7d70d6a04e9eca4e27baaaf30a62deb6587fbe
lorybst <lory.bastimar@hotmail.de>
2017-01-05 11:25:41 +0100

    chatbot

---
+++ bot, informatik.py
@@ -0,0 +1,23 @@
+
+
+input("Hallo Mensch\n")
+
+
+input("Gut und dir\n")
+
+
+input("Auch gut\n")
+
+
+input("Wo kommst du her?\n")
+
+
+input("Sag ich dir nicht\n")
+
+
+input("Ich vertraue dir nicht\n")
+
+      
+
+    
+
